2bdf7228a28d7d0353acf7d00a1f33c8ec51170e
Everlyn <gachuieve32@gmail.com>
2024-01-20 15:13:07 +0300

    done

---
+++ server/migrations/versions/dbcd0dc2c803_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: dbcd0dc2c803
+Revises: c93a14d1aa19
+Create Date: 2024-01-20 14:46:48.477865
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'dbcd0dc2c803'
+down_revision = 'c93a14d1aa19'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
